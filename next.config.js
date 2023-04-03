659a4f058be1cf533ddde1619ae2a86da03930be
maivl <wkf39988@gmail.com>
2023-04-03 16:32:07 +0800

    feat: update

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/c33f0f64-9add-4351-ac8c-c869d382d4f8-bucket/civia-metamask-connect-dapp',
+  // basePath: '/c33f0f64-9add-4351-ac8c-c869d382d4f8-bucket/civia-metamask-connect-dapp',
 }
