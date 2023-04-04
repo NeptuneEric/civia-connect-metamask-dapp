54c50cfa56e82e3f223835e4e66af773ebe16292
maivl <wkf39988@gmail.com>
2023-04-04 11:38:53 +0800

    cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,7 +19,8 @@ export default async function getSessionToken(
             mode: 'cors',
             headers: {
                 "Content-Type": "application/json",
-                ...request.headers
+                ...request.headers,
+                origin: ''
             },
             body: request.body || "{}"
         });
@@ -31,7 +32,8 @@ export default async function getSessionToken(
             mode: 'cors',
             headers: {
                 "Content-Type": "application/json",
-                ...request.headers
+                ...request.headers,
+                origin: ''
             },
             body: request.body || "{}"
         });
